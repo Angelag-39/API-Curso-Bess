34c2cbd1e8b47735763b519bdb6d5fb9f1ccb3dd
angela <angelaguacaran@gmail.com>
2023-05-10 22:29:46 -0300

    api-nasa

---
+++ api-nasa/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImagenNasa from './ImagemNasa'
+
+function App() {
+  return (
+    <div className="App">
+     <ImagenNasa/>
+    </div>
+  );
+}
+
+export default App;
